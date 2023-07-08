c430228f5dc347b8f8f2737b659ef1c5f9a036f9
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-07-08 01:15:37 -0500

    🔒️ singleton

--- libs/design-patterns/src/lib/singleton.ts
+++ libs/design-patterns/src/lib/singleton.ts
@@ -1,8 +1,37 @@
 export class Singleton {
 
-  private static instance: Singleton;
+  private static instance: Singleton | null = null;
 
-  private constructor() {
+  private constructor() { }
 
+  public static getInstance() {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+
+    return Singleton.instance;
   }
 }
+
+export default Singleton.getInstance();
+
+
+export class UsersAPISingleton {
+  private static instance: UsersAPISingleton;
+
+  private constructor() { }
+
+  static getInstance() {
+    if (!UsersAPISingleton.instance) {
+      UsersAPISingleton.instance = new UsersAPISingleton();
+    } return UsersAPISingleton.instance;
+  }
+
+  getUsers(): Promise<string[]> {
+    return Promise.resolve(["Alex", "John", "Sarah"]);
+  }
+}
+
+const usersPromise = UsersAPISingleton.getInstance().getUsers();
+
+usersPromise.then((res) => { console.log(res); });
