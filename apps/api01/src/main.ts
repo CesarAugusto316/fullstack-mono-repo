c42a682f38becef4b340fc362753acb02382b053
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-11-21 07:53:46 -0500

    ✨ microservices beginning

--- apps/api01/src/main.ts
+++ apps/api01/src/main.ts
@@ -1,33 +1,36 @@
-// import express from 'express';
-import { Server, ServerOptions } from 'https'
+import express from 'express';
+import * as fs from 'fs/promises'
+import { createReadStream } from 'fs'
+
 
 const HOST = process.env.HOST ?? 'localhost';
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const options = {
-  // keepAlive: true,
-  // enableTrace: true
-} as ServerOptions
 
-const server = new Server(options, (req, res) => {
-  if (req.method === 'GET') {
-    console.log(req)
-  }
+const app = express();
 
-  res.end()
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
 });
 
 
-server.listen(PORT, HOST, () => {
+app.get('/video', async (req, res) => {
+  try {
+    const videoPath = 'apps/api01/src/assets/SampleVideo_1280x720_1mb.mp4';
+    const stats = await fs.stat(videoPath)
+    res.writeHead(200, {
+      'Content-Length': stats.size,
+      'Content-Type': 'video/mp4'
+    });
+
+    createReadStream(videoPath).pipe(res)
+  }
+  catch (error) {
+    res.status(400).json({ error: error.name })
+  }
+});
+
+
+app.listen(PORT, HOST, () => {
   console.log(`[ ready ] http://${HOST}:${PORT}`);
-})
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.send({ message: 'Hello API' });
-// });
-
-// app.listen(PORT, HOST, () => {
-//   console.log(`[ ready ] http://${HOST}:${PORT}`);
-// });
+});
